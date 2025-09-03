60ca58b89eadf0956c6580d03960f7a02ac3dccc
LearningMasterA <ankitasingh0263@gmail.com>
2025-09-03 14:12:38 +0530

    email validation added

--- src/main/java/com/practice/controller/MainController.java
+++ src/main/java/com/practice/controller/MainController.java
@@ -2,11 +2,14 @@ package com.practice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.practice.entities.loginData;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class MainController {
@@ -17,7 +20,11 @@ public class MainController {
 		return "form";
 	}
 	@PostMapping("/process")
-	public String processForm(@ModelAttribute("loginData") loginData loginData) {
+	public String processForm(@Valid @ModelAttribute("loginData") loginData loginData,BindingResult result) {
+		if(result.hasErrors()) {
+			System.out.println(result);
+			return "form";
+		}
 		System.out.println(loginData);
 		return "success";
 	}
--- src/main/java/com/practice/entities/loginData.java
+++ src/main/java/com/practice/entities/loginData.java
@@ -1,13 +1,18 @@
 package com.practice.entities;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class loginData {
 	@NotBlank(message="User name cannot be empty")
-	@Min
+	@Size(min=3,max=12,message="Username must be between 3-12 characters")
 	private String userName;
-	@NotBlank(message="email cannot be empty")
+	
+//	@NotBlank(message="email cannot be empty")
+	@Email(regexp ="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$",message="invalid email !! " )
 	private String email;
 	/**
 	 * 
